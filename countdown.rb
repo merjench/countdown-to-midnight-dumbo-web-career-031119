a1ba8a8937b7c050ed868aa023e917b91baab59d
Merjen Charyyeva <merjen.chary@gmail.com>
2019-03-05 16:57:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(numb)
+  while numb > 0 
+  puts "#{numb} SECOND(S)!"
+    numb -=1 
+end 
+ "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(numb)
+  while numb > 0 
+  puts "#{numb} SECOND(S)!"
+  sleep(1)
+    numb -=1 
+end 
+ "HAPPY NEW YEAR!"
 end
